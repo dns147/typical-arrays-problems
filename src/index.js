605c404ca9b06cb35ae870ac0385b57d911cedbe
github-dns147 <rarden@ya.ru>
2021-08-08 17:21:43 +0300

    add: solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,47 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array || !array.length) return 0;
+
+  const len = array.length;
+  let min = array[0];
+
+  for (i = 0; i < len; i++) {
+    for (k = 0; k < len; k++) {
+      if (array[i] < array[k] && array[i] < min) {
+        min = array[i];
+      }
+    }
+  }
+
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || !array.length) return 0;
+
+  const len = array.length;
+  let max = array[0];
+
+  for (i = 0; i < len; i++) {
+    for (k = 0; k < len; k++) {
+      if (array[i] > array[k] && array[i] > max) {
+        max = array[i];
+      }
+    }
+  }
+
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || !array.length) return 0;
+
+  const len = array.length;
+  let avg = null;
+
+  for (i = 0; i < len; i++) {
+    avg = avg + array[i];
+  }
+
+  return avg / len;
 }
